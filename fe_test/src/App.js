f9caa1d3118afd58753508732c2913411b6db533
ignacio <iggirex@gmail.com>
2018-03-23 16:21:25 -0600

    added first container and Routes

--- fe_test/src/App.js
+++ fe_test/src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'semantic-ui-react'
+import Routes from './Routes';
 
-const ButtonExampleButton = () => (
-  <Button>Click Here Igs</Button>
-)
+// import { Button } from 'semantic-ui-react';
+import './App.css'
+import MenuExampleBasic from './components/Menu';
+
+// const ButtonExampleButton = () => (
+//   <Button>Click Here Igs</Button>
+// )
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ButtonExampleButton />
+      // App container making NAV narrow
+      <div className="App container">
+        <MenuExampleBasic />
+        <Routes />
+        {/* <ButtonExampleButton /> */}
       </div>
     );
   }
---
+++ fe_test/src/components/Menu.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+export default class MenuExampleBasic extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu>
+        <Menu.Item
+          name='editorials'
+          active={activeItem === 'editorials'}
+          onClick={this.handleItemClick}
+        >
+          <Link to="/">Editorials</Link>
+
+        </Menu.Item>
+
+        <Menu.Item
+          name='reviews'
+          active={activeItem === 'reviews'}
+          onClick={this.handleItemClick}
+        >
+          Reviews
+        </Menu.Item>
+
+        <Menu.Item
+          name='upcomingEvents'
+          active={activeItem === 'upcomingEvents'}
+          onClick={this.handleItemClick}
+        >
+          Upcoming Events
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
